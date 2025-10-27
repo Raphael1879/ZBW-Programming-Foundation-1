6e48f30a336a14d0fde46bc8440fa191f1642526
Lord_Stamm <stamm.raphael17@gmail.com>
2025-10-27 20:16:55 +0100

    feat: create the best version of pong ever

---
+++ Module2/Pong/Pong.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Pong
+{
+    partial class Pong
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            ball = new Panel();
+            startButton = new Button();
+            player = new Panel();
+            scoreLabel = new Label();
+            levelLabel = new Label();
+            SuspendLayout();
+            // 
+            // ball
+            // 
+            ball.BackColor = Color.Red;
+            ball.Location = new Point(12, 58);
+            ball.Name = "ball";
+            ball.Size = new Size(25, 25);
+            ball.TabIndex = 0;
+            // 
+            // startButton
+            // 
+            startButton.Dock = DockStyle.Top;
+            startButton.Location = new Point(0, 0);
+            startButton.Name = "startButton";
+            startButton.Size = new Size(800, 23);
+            startButton.TabIndex = 1;
+            startButton.Text = "Start";
+            startButton.UseVisualStyleBackColor = true;
+            startButton.Click += startButton_Click;
+            // 
+            // player
+            // 
+            player.Anchor = AnchorStyles.Right;
+            player.BackColor = Color.DodgerBlue;
+            player.Location = new Point(736, 156);
+            player.Name = "player";
+            player.Size = new Size(25, 100);
+            player.TabIndex = 1;
+            // 
+            // scoreLabel
+            // 
+            scoreLabel.AutoSize = true;
+            scoreLabel.Dock = DockStyle.Left;
+            scoreLabel.Location = new Point(0, 23);
+            scoreLabel.Name = "scoreLabel";
+            scoreLabel.Size = new Size(48, 15);
+            scoreLabel.TabIndex = 2;
+            scoreLabel.Text = "Score: 0";
+            // 
+            // levelLabel
+            // 
+            levelLabel.AutoSize = true;
+            levelLabel.Dock = DockStyle.Left;
+            levelLabel.Location = new Point(48, 23);
+            levelLabel.Name = "levelLabel";
+            levelLabel.Size = new Size(46, 15);
+            levelLabel.TabIndex = 3;
+            levelLabel.Text = "Level: 1";
+            // 
+            // Pong
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(levelLabel);
+            Controls.Add(scoreLabel);
+            Controls.Add(player);
+            Controls.Add(startButton);
+            Controls.Add(ball);
+            Name = "Pong";
+            Text = "Pong";
+            MouseMove += Pong_MouseMove;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel ball;
+        private Button startButton;
+        private Panel player;
+        private Label scoreLabel;
+        private Label levelLabel;
+    }
+}
---
+++ Module2/Pong/Pong.cs
@@ -0,0 +1,117 @@
+﻿
+namespace Pong
+{
+    public partial class Pong : Form
+    {
+        private System.Windows.Forms.Timer _gameTimer;
+        private int _xVelocity;
+        private int _yVelocity;
+        private int _score;
+        private int _level;
+
+
+        public Pong()
+        {
+            InitializeComponent();
+        }
+
+
+        private void StartGameLoop()
+        {
+            ConfigureGame();
+            _gameTimer = new System.Windows.Forms.Timer();
+            _gameTimer.Interval = 1;
+            _gameTimer.Tick += GameLoop;
+            _gameTimer.Start();
+        }
+
+        private void ConfigureGame()
+        {
+            var random = new Random();
+            _xVelocity = random.Next(1,3);
+            _yVelocity = random.Next(1, 3); ;
+            _score = 0;
+            _level = 1;
+            levelLabel.Text = $"Level: {_level.ToString()}";
+            scoreLabel.Text = $"Score: {_score.ToString()}";
+        }
+
+        private void GameLoop(object sender, EventArgs e)
+        {
+
+            ball.Location = new Point
+            {
+                X = ball.Location.X + _xVelocity * _level,
+                Y = ball.Location.Y + _yVelocity * _level
+            };
+
+            var playerHit = ball.Bounds.IntersectsWith(player.Bounds);
+            var gameOver = ball.Right >= ClientSize.Width;
+            if (ball.Left <= 0 || gameOver || playerHit)
+            {
+                _xVelocity = -_xVelocity; // reverse X direction
+
+                if(playerHit)
+                {
+                    OnPlayerHit();
+                }
+
+                if(gameOver)
+                {
+                    Gameover();
+                }
+            }
+
+            // --- Collision with top or bottom edge ---
+            if (ball.Top <= 0 || ball.Bottom >= ClientSize.Height)
+            {
+                _yVelocity = -_yVelocity; // reverse Y direction
+            }
+        }
+
+        private void OnPlayerHit()
+        {
+            _score++;
+            scoreLabel.Text = $"Score: {_score.ToString()}";
+
+            if(_score % 3 == 0)
+            {
+                //increase Level
+                _level++;
+                levelLabel.Text = $"Level: {_level.ToString()}";
+            }
+        }
+
+        private void Gameover()
+        {
+            _gameTimer.Stop();
+            MessageBox.Show("Game Over!");
+            startButton.Visible = true;
+
+            ball.Location = new Point
+            {
+                X = 15,
+                Y = 15
+            };
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            startButton.Visible = false;
+            StartGameLoop();
+        }
+
+        private void Pong_MouseMove(object sender, MouseEventArgs e)
+        {
+            int newY = e.Location.Y - player.Height / 2;
+
+            // Clamp Y position so the paddle stays inside the window
+            if (newY < 0)
+                newY = 0;
+            else if (newY + player.Height > ClientSize.Height)
+                newY = ClientSize.Height - player.Height;
+
+            player.Location = new Point(player.Location.X, newY);
+        }
+    }
+}
